240a9c3deacdfa431d4e2c6b2cb53567347090da
yugal-compro <yugal.vashishth@comprotechnologies.com>
2024-01-15 13:10:42 +0530

    newrelic added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      __dangerouslyDisableSanitizers: ["script"],
+    script: [
+      {
+        hid: "run",
+        src: "newrelic.js",
+        defer: true,
+        type: "text/javascript",
+        charset: "utf-8",
+      },
+    ],
+    },
+  },
 })
